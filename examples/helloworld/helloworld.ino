1ab884e939d504b2d7d872bb5b2b42e7299d1073
dragula96 <dragula96@users.noreply.github.com>
2015-06-12 11:57:25 -0500

    Update helloworld.ino

--- examples/helloworld/helloworld.ino
+++ examples/helloworld/helloworld.ino
@@ -20,7 +20,7 @@ version 2.1 of the License, or (at your option) any later version.
 Arduboy arduboy;
 
 // Variables for your game go here.
-char PROGMEM text[] = "Hello, World!";
+char text[] = "Hello, World!";
 
 
 // This function runs once in your game.
