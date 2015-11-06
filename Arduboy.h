727df2ebbfb6e5a8394bff62d57ed9d51bfbc109
rogosher <shogerr@onid.oregonstate.edu>
2015-11-06 08:25:07 -0800

    Update Arduboy.h

--- Arduboy.h
+++ Arduboy.h
@@ -130,12 +130,12 @@ public:
   /// Sets the location of the screen cursor.
   void setCursor(int16_t x, int16_t y);
 
-   /// Sets the text size
-   /**
-    * As mentioned in drawChar(), individual ASCII characters are 6x8 pixels
-    * (5x7 with spacing on two edges). The size is a pixel multiplier,
-    * so a size of 2 means each character will be 12x16, etc.
-    */
+  /// Set text size
+  /**
+   * As mentioned in drawChar(), individual ASCII characters are 6x8 pixels
+   * (5x7 with spacing on two edges). The size is a pixel multiplier,
+   * so a size of 2 means each character will be 12x16, etc.
+   */
   void setTextSize(uint8_t s);
 
   /// Sets whether text will wrap at screen edges.
@@ -147,10 +147,10 @@ public:
   virtual size_t write(uint8_t);
 
   /// Seed random.
-	/**
+  /**
    * the random number generator with entropy from the temperature,
    * voltage reading, and microseconds since boot.
-	 */
+   */
   void initRandomSeed();
 
   /// Swap the references of two pointers.
