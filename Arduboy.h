4178505eb7f59db62d73608e345108a5f503804a
rogosher <shogerr@onid.oregonstate.edu>
2015-11-06 08:36:29 -0800

    Update Arduboy.h

--- Arduboy.h
+++ Arduboy.h
@@ -56,7 +56,7 @@ public:
    * is the bottom of the screen and an X of 128 is the right side of the screen.
    * "Color" or "value" means choosing whether a pixel is lit or not - if color is
    * 0, the pixel is off (black), if color is 1, the pixel is on (white).
-	 */
+   */
   void display();
 
   /// Sets a single pixel on the screen buffer to white or black.
