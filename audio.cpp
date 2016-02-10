3ece19b3104599b04bfcad3110d0492847a02879
Josh Goebel <dreamer3@gmail.com>
2016-02-10 08:33:56 -0500

    reduce storage of note frequences (-32 bytes)

--- audio.cpp
+++ audio.cpp
@@ -24,10 +24,13 @@ volatile const byte *score_cursor = 0;
 //   Generated from Excel by =ROUND(2*440/32*(2^((x-9)/12)),0) for 0<x<128
 // The lowest notes might not work, depending on the Arduino clock frequency
 // Ref: http://www.phy.mtu.edu/~suits/notefreqs.html
-const unsigned int PROGMEM _midi_note_frequencies[128] = {
+const uint8_t PROGMEM _midi_byte_note_frequencies[48] = {
 16,17,18,19,21,22,23,24,26,28,29,31,33,35,37,39,41,44,46,49,52,55,58,62,65,
 69,73,78,82,87,92,98,104,110,117,123,131,139,147,156,165,175,185,196,208,220,
-233,247,262,277,294,311,330,349,370,392,415,440,466,494,523,554,587,622,659,
+233,247
+};
+const unsigned int PROGMEM _midi_word_note_frequencies[80] = {
+262,277,294,311,330,349,370,392,415,440,466,494,523,554,587,622,659,
 698,740,784,831,880,932,988,1047,1109,1175,1245,1319,1397,1480,1568,1661,1760,
 1865,1976,2093,2217,2349,2489,2637,2794,2960,3136,3322,3520,3729,3951,4186,
 4435,4699,4978,5274,5588,5920,6272,6645,7040,7459,7902,8372,8870,9397,9956,
@@ -121,7 +124,11 @@ void ArduboyTunes::playNote(byte chan, byte note)
     note = 127;
 
   timer_num = pgm_read_byte(tune_pin_to_timer_PGM + chan);
-  frequency2 = pgm_read_word(_midi_note_frequencies + note);
+  if (note < 48) {
+    frequency2 = pgm_read_byte(_midi_byte_note_frequencies + note);
+  } else {
+    frequency2 = pgm_read_word(_midi_word_note_frequencies + note - 48);
+  }
 
   //******  16-bit timer  *********
   // two choices for the 16 bit timers: ck/1 or ck/64
